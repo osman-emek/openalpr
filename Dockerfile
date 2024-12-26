eb25d8476cb26d6b83d751f5d782bf433f147257
Osman Emek <osman@geonni.com>
2024-12-26 15:31:21 +0300

    update dockerfile and main.py

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 from ubuntu:18.04
 
-# Install prerequisites
 run apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     build-essential \
     cmake \
@@ -12,34 +11,44 @@ run apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     libopencv-dev \
     libtesseract-dev \
     wget \
-    software-properties-common \
-    python3 \
-    python3-pip
+    software-properties-common
 
-# Copy all data
 copy . /srv/openalpr
 
-# Setup the build directory
 run mkdir /srv/openalpr/src/build
 workdir /srv/openalpr/src/build
 
-# Setup the compile environment
 run cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_SYSCONFDIR:PATH=/etc .. && \
     make -j2 && \
     make install
 
+#///////
+
+run add-apt-repository -y ppa:deadsnakes/ppa
+
+run apt-get update && apt-get install -y python3.8
+run apt-get install -y python3-pip
+
+run python3.8 -m pip --no-cache-dir install pip
+
+run apt-get install -y python3-distutils
+run apt-get install -y python3-setuptools
+run apt-get install -y python3.8-venv
+
+run python3.8 -m venv /venv
+
+env PATH="/venv/bin:$PATH"
+
+run python3.8 -m pip install --no-cache-dir pip --upgrade pip
+
 run mkdir /opt/app
 workdir /opt/app
 copy ./code/main.py /opt/app/main.py
-#run python -m venv /venv
-#env PATH="/venv/bin:$PATH"
-run pip3 install --upgrade pip
-run pip3 install openalpr
-run pip3 install tornado
-run pip3 install numpy
+copy ./code/requirements.txt /opt/app/requirements.txt
+
+run pip3 install --no-cache-dir fastapi
+run pip3 install --no-cache-dir fastapi-cli
 
 expose 7878
 
-cmd ["python3", "/opt/app/main.py"]
-
-#entrypoint ["alpr"]
+cmd ["fastapi", "run", "/opt/app/main.py","--port","7878","--workers","4","--host","0.0.0.0"]
--- code/main.py
+++ code/main.py
@@ -1,82 +1,71 @@
 import json
 import base64
 import os
+import logging
+from typing import Dict, Any
 
-import tornado.ioloop
-import tornado.web
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
-import subprocess  
+import subprocess
 
-class MainHandler(tornado.web.RequestHandler):
-    def post(self):
-        try:
-            payload = json.loads(self.request.body)
-        except json.JSONDecodeError:
-            self.set_status(400)
-            self.finish('Invalid JSON payload')
-            return
+VERSION = 0.2
 
-        if 'base64' not in payload:
-            self.set_status(400)
-            self.finish('"base64" parameter not provided')
-            return
+app = FastAPI(debug=True)
 
-        base64_string = payload.get('base64')
-        try:
-            jpeg_bytes = base64.b64decode(base64_string)
-        except base64.binascii.Error:
-            self.set_status(400)
-            self.finish('Invalid base64 encoding')
-            return
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
-        country = payload.get('country', '')
-        procid = payload.get('procid', '')
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-        output_dir = "uploads"  
-        os.makedirs(output_dir, exist_ok=True)
-        file_name = os.path.join(output_dir, "uploaded_image_"+procid+".jpg")
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-        try:
-            with open(file_name, "wb") as f:
-                f.write(jpeg_bytes)
-        except Exception as e:
-            self.set_status(500)
-            self.finish(f'Error saving image to disk: {str(e)}')
-            return
+@app.post("/alpr")
+async def alpr(payload: Dict[Any, Any]):
+    try:
+        base64Data = payload.get("base64")
+        jpegData = base64.b64decode(base64Data)
 
-        process = subprocess.Popen(
-            ['/usr/bin/alpr', '-c', country, '-n', '1', '-j', file_name],
+        country = payload.get("country")
+        procid = payload.get("procid")
+
+        imgFilename = os.path.join(UPLOAD_FOLDER, f"pimg_{procid}.jpg")
+        with open(imgFilename, "wb") as f:
+            f.write(jpegData)
+
+        osProc = subprocess.Popen(
+            ['/usr/bin/alpr', '-c', country, '-n', '1', '-j', imgFilename],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
-        )        
+        )
 
-        stdout, stderr = process.communicate()
+        stdout, stderr = osProc.communicate()
 
-        if process.returncode == 0:
+        if osProc.returncode == 0:
             try:
-                if os.path.exists(file_name):
-                    os.remove(file_name)
+                if os.path.exists(imgFilename):
+                    os.remove(imgFilename)
 
-                result = json.loads(stdout)
-                self.finish(json.dumps(result))
-            except json.JSONDecodeError as e:
-                result = {"status":False,"error":e.msg}
-                self.finish(json.dumps(result))
+                return json.loads(stdout)
+            except:
+                return {"status": False, "results": []}
         else:
-            result = {"status":False,"error":stderr.decode('utf-8')}
-            self.finish(json.dumps(result))            
+            return {"status": False}
+    except:
+        return {"status": False, "results": []}
 
-class CheckHandler(tornado.web.RequestHandler):
-    def get(self):
-        results = {"status": "OK"}
-        self.finish(json.dumps(results))
+@app.get("/check")
+async def check():
+    return {"status": "OK"}
 
-
-application = tornado.web.Application([
-    (r"/alpr", MainHandler),
-    (r"/check", CheckHandler),
-])
-
-if __name__ == "__main__":
-    application.listen(7878)
-    tornado.ioloop.IOLoop.current().start()
+@app.get("/version")
+async def version():
+    return {"versions": str(VERSION)}
